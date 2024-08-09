68691246d51314e2d045c768a440cc09e06c13d1
Lina <linaoeu567556@gmail.com>
2024-08-09 17:40:21 +0700

    Done

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // write your code below
+
+for (let i = 1; i <=100; i++) {
+  // For numbers divisible by 3 and also divisible by 5, print `FizzBuzz`
+  if (i%3==0 && i%5==0) {
+    console.log("FizzBuzz");
+  // For numbers divisible by 3 but not divisible by 5, print `Fizz`
+  }else if(i%3==0){
+    console.log("Fizz");
+  }
+  // For numbers divisible by 5 but not divisible by 3, print `Buzz`
+  else if(i%5==0){
+    console.log("Buzz");
+  // Other cases, just print the number in string format.
+  }else{
+    console.log(i.toString());
+  }
+  
+}
